32a65bee19a72327f697b2c22e81feab5ef4dbf1
DrHistory <kursatkumsuz@gmail.com>
2023-03-14 20:44:00 +0300

    Initial commit

---
+++ mylibrary/src/main/java/com/fearlessfigter/mylibrary/CustomText.kt
@@ -0,0 +1,9 @@
+package com.fearlessfigter.mylibrary
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomText(){
+    Text("This is my Library")
+}
